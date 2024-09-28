4ef940ac60c934eb40080706fea331e8beea41b0
alexander <alexandersomba17@gmail.com>
2024-09-28 19:41:29 +0300

    details screen for exercises

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,15 +1,19 @@
-import { StyleSheet, Text, View, } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function ExerciseListItems({ item }){
-    return  (
-      <View style={styles.exerciseContainer}>
+  return  (
+    <Link href={`/${item.name}`} asChild>
+      <Pressable style={styles.exerciseContainer}>
         <Text style={styles.exerciseName}>{item.name}</Text>
+        
         <Text style={styles.exerciseSubtitle}>
           <Text style={styles.subValue}>{item.muscle}</Text> |{' '}
           <Text style={styles.subValue}>{item.equipment}</Text>
         </Text>
-      </View>
-    )
+      </Pressable>
+    </Link>
+  )
 }
 
 const styles = StyleSheet.create({
