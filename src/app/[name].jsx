1ccc1d537a815161d1647c47d7db53b02a104e6b
ale-ship <alexandersomba17@gmail.com>
2025-03-15 23:17:55 +0300

    new set input UI

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { gql } from 'graphql-request';
 import { useQuery } from '@tanstack/react-query';
 import graphqlClient from '../graphqlClient';
+import NewSetInput from '../components/NewSetInput';
 
 const exerciseQuery = gql`
     query exercises($name: String) {
@@ -68,6 +69,8 @@ export default function ExerciseDetailsScreen(){
                     {isInstructionExpand ? 'See less' : 'See more'}
                 </Text>
             </View>
+
+            <NewSetInput/>
         </ScrollView>
     );
 }
